6759381a60b80c9710c230fba7e3e5712affa623
Ubuntu <ubuntu@ip-172-31-23-59.us-east-2.compute.internal>
2021-06-18 05:22:03 +0000

    added siemens program

---
+++ src/test/java/siemens/logintest.java
@@ -0,0 +1,21 @@
+package siemens;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class logintest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
+
